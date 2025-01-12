956381d04555ce562af1af0912d5d60708c9c3fb
Uttam Mahata <92252205+Uttam-Mahata@users.noreply.github.com>
2025-01-12 09:50:14 +0000

    revert

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
+# app.py
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
-import threading
-import time
-import requests
 
 from config import Config
 from routes import api_bp
 from extensions import db  # Import db from extensions
 
+
 load_dotenv()
 
 app = Flask(__name__)
+#CORS(app)
+# Implement cors for specific origin and methods
+CORS(app, resources={r"/api/*": {"origins": "https://uttamm.web.app/"}, r"/api/*": {"methods": "POST"}})
 
-# Define a list of allowed origins for CORS
-allowed_origins = ["https://uttamm.web.app", "https://contact-form-v2ph.onrender.com"]  # Add more as needed
-
-# Configure CORS with multiple origins
-CORS(app, resources={r"/api/*": {"origins": allowed_origins}},  methods=["POST"])
 
 app.config.from_object(Config)
 db.init_app(app)
@@ -28,36 +25,6 @@ with app.app_context():
 # Register the API blueprint
 app.register_blueprint(api_bp, url_prefix='/api')
 
-def ping_self():
-    while True:
-        try:
-            with app.app_context():
-                response = requests.get('https://contact-form-v2ph.onrender.com/api/test')
-                print(f'Self-Ping Status: {response.status_code}')
-        except requests.exceptions.RequestException as e:
-            print(f'Self-Ping Error: {e}')
-        time.sleep(1000)
-
-ping_thread = None
-ping_thread_started = False  # Track if ping thread is already running
-
-
-@app.before_first_request
-def before_first_request():
-    global ping_thread, ping_thread_started
-    if not ping_thread_started:
-        ping_thread = threading.Thread(target=ping_self, daemon=True)
-        ping_thread.start()
-        ping_thread_started = True  # Set flag to true once started
-
-
-
-@app.teardown_appcontext
-def teardown_appcontext(exception=None):
-    global ping_thread
-    if ping_thread and ping_thread.is_alive():
-        ping_thread.join(1)
-
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)  # Allow external access with host and port, use 0.0.0.0 for external traffic
+    app.run(debug=True)

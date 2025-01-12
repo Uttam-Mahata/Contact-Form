87e4fc1b02a09100f5e0e7fcbab39ec9513764b5
Uttam Mahata <92252205+Uttam-Mahata@users.noreply.github.com>
2025-01-12 09:44:14 +0000

    ping test

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-# app.py
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
+import threading
+import time
+import requests
 
 from config import Config
 from routes import api_bp
@@ -11,8 +13,7 @@ from extensions import db  # Import db from extensions
 load_dotenv()
 
 app = Flask(__name__)
-#CORS(app)
-# Implement cors for specific origin and methods
+
 CORS(app, resources={r"/api/*": {"origins": "https://uttamm.web.app/"}, r"/api/*": {"methods": "POST"}})
 
 
@@ -24,6 +25,32 @@ with app.app_context():
 
 # Register the API blueprint
 app.register_blueprint(api_bp, url_prefix='/api')
+
+def ping_self():
+    while True:
+        try:
+            with app.app_context():
+              response = requests.get('https://contact-form-v2ph.onrender.com/api/test')
+              print(f'Self-Ping Status: {response.status_code}')
+        except requests.exceptions.RequestException as e:
+            print(f'Self-Ping Error: {e}')
+        time.sleep(1000)
+
+ping_thread = None
+
+@app.before_first_request
+def before_first_request():
+    global ping_thread
+    if ping_thread is None:
+        ping_thread = threading.Thread(target=ping_self, daemon=True)
+        ping_thread.start()
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception=None):
+    global ping_thread
+    if ping_thread and ping_thread.is_alive():
+        ping_thread.join(1)
 
 
 if __name__ == '__main__':

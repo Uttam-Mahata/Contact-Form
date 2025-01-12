0221c334b0fe061ac1385b015110442b131552f7
Uttam Mahata <92252205+Uttam-Mahata@users.noreply.github.com>
2025-01-12 09:47:00 +0000

    ping cros origin

--- app.py
+++ app.py
@@ -9,13 +9,15 @@ from config import Config
 from routes import api_bp
 from extensions import db  # Import db from extensions
 
-
 load_dotenv()
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/api/*": {"origins": "https://uttamm.web.app/"}, r"/api/*": {"methods": "POST"}})
+# Define a list of allowed origins for CORS
+allowed_origins = ["https://uttamm.web.app", "https://contact-form-v2ph.onrender.com"]  # Add more as needed
 
+# Configure CORS with multiple origins
+CORS(app, resources={r"/api/*": {"origins": allowed_origins}},  methods=["POST"])
 
 app.config.from_object(Config)
 db.init_app(app)
@@ -30,20 +32,24 @@ def ping_self():
     while True:
         try:
             with app.app_context():
-              response = requests.get('https://contact-form-v2ph.onrender.com/api/test')
-              print(f'Self-Ping Status: {response.status_code}')
+                response = requests.get('https://contact-form-v2ph.onrender.com/api/test')
+                print(f'Self-Ping Status: {response.status_code}')
         except requests.exceptions.RequestException as e:
             print(f'Self-Ping Error: {e}')
         time.sleep(1000)
 
 ping_thread = None
+ping_thread_started = False  # Track if ping thread is already running
+
 
 @app.before_first_request
 def before_first_request():
-    global ping_thread
-    if ping_thread is None:
+    global ping_thread, ping_thread_started
+    if not ping_thread_started:
         ping_thread = threading.Thread(target=ping_self, daemon=True)
         ping_thread.start()
+        ping_thread_started = True  # Set flag to true once started
+
 
 
 @app.teardown_appcontext
@@ -54,4 +60,4 @@ def teardown_appcontext(exception=None):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)  # Allow external access with host and port, use 0.0.0.0 for external traffic
